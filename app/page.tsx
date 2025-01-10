6c565fe13fd6a9341c5a6be11e2cabd4456f838b
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-01-10 13:50:22 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ArbeitszeitRechner from '@/components/ArbeitszeitRechner';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <ArbeitszeitRechner />
+    </main>
+  );
+}
