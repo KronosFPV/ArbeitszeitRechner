96f4a6cdd0b79751ccaeaf753869866d20969b33
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-01-10 13:52:05 +0100

    Delete Components/ui directory

--- Components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-import ArbeitszeitRechner from '@/components/ArbeitszeitRechner';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-8">
-      <ArbeitszeitRechner />
-    </main>
-  );
-}
